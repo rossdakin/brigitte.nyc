c5ca01bcf0bc49cda07c053760ebf309482c7173
Ross Dakin <rossdakin@gmail.com>
2015-10-23 22:31:02 -0400

    Try preloading images

--- makeboxes.js
+++ makeboxes.js
@@ -1,6 +1,9 @@
 var IMAGE_COUNT = 47; // 0-indexed filenames
 var MIN_WIDTH = 204 //($(window).width() / 3);
+var INIT_COUNT = 15;
+var NEW_COUNT = 2;
 var reservedNumbers = {};
+var preloadedNumbers = [];
 
 function randomColor() {
   return '#' +
@@ -9,12 +12,32 @@ function randomColor() {
     Math.floor(Math.random() * 256).toString(16);
 }
 
+function buildImagePath(n) {
+  return 'images/' + n + '.jpg';
+}
+
+function preloadBoxes(count, callback) {
+  var n = reserveNumber();
+  var image = new Image();
+  image.src = buildImagePath(n);
+
+  preloadedNumbers.push(n);
+
+  if (callback) {
+    callback();
+  }
+}
+
+function fetchReservedNumber() {
+  return preloadedNumbers.pop() || reserveNumber();
+}
+
 function makeBoxes(count) {
   var boxes = [];
 
   for (var i = 0; i < count; i++) {
     var box = document.createElement('div');
-    var n = reserveNumber();
+    var n = fetchReservedNumber();
 
     if (!n) {
       window.clearInterval(window.interval);
@@ -25,7 +48,7 @@ function makeBoxes(count) {
       Math.ceil(Math.random() * 3) +
       Math.ceil(Math.random() * 2);
 
-    box.style.backgroundImage = 'url("images/' + n + '.jpg")';
+    box.style.backgroundImage = 'url("' + buildImagePath(n) + '")';
 
     boxes.push(box);
   }
@@ -50,21 +73,25 @@ function reserveNumber() {
 }
 
 $(function() {
-  var initBoxes = makeBoxes(15);
-  $('#container')
-    .prepend(initBoxes)
-    .nested({
-      animate: true,
-      animationOptions: {
-        speed: 100,
-        duration: 80
-      },
-      minWidth: MIN_WIDTH
-    })
-    .nested('prepend', initBoxes);
+  preloadBoxes(INIT_COUNT, function() {
+    var initBoxes = makeBoxes(INIT_COUNT);
+    $('#container')
+      .prepend(initBoxes)
+      .nested({
+        animate: true,
+        animationOptions: {
+          speed: 100,
+          duration: 80
+        },
+        minWidth: MIN_WIDTH
+      })
+      .nested('prepend', initBoxes);
 
-  window.interval = window.setInterval(function() {
-    var boxes = makeBoxes(2);
-    $('#container').prepend(boxes).nested('prepend', boxes);
-  }, 5000);
+    preloadBoxes(NEW_COUNT)
+    window.interval = window.setInterval(function() {
+      var boxes = makeBoxes(NEW_COUNT);
+      $('#container').prepend(boxes).nested('prepend', boxes);
+      preloadBoxes(NEW_COUNT)
+    }, 5000);
+  });
 });
